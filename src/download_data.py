4a95e2a291a175ae7fa5ed1b872fff6f7cee60d3
rajiv.sambasivan@gmail.com <rajiv.sambasivan@arangodb.com>
2022-05-13 14:18:39 +0530

    stop tracking src and data

--- src/download_data.py
+++
@@ -1,6 +0,0 @@
-import requests, zipfile, io, os
-r = requests.get('https://files.grouplens.org/datasets/movielens/ml-100k.zip', verify=False)
-z = zipfile.ZipFile(io.BytesIO(r.content))
-path_parent = os.path.dirname(os.getcwd())
-data_dir = path_parent + '/data'
-z.extractall(data_dir)
