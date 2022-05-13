f0fd9311bf9351c094a81a4fe7a709e697075125
rajiv.sambasivan@gmail.com <rajiv.sambasivan@arangodb.com>
2022-05-13 14:13:15 +0530

    gitignore fix

--- src/download_data.py
+++ src/download_data.py
@@ -1,5 +1,5 @@
 import requests, zipfile, io, os
-r = requests.get('https://files.grouplens.org/datasets/movielens/ml-100k.zip')
+r = requests.get('https://files.grouplens.org/datasets/movielens/ml-100k.zip', verify=False)
 z = zipfile.ZipFile(io.BytesIO(r.content))
 path_parent = os.path.dirname(os.getcwd())
 data_dir = path_parent + '/data'
