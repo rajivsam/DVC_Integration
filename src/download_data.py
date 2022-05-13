12177f75d03cb468e13e389801cce9a31230b812
rajiv.sambasivan@gmail.com <rajiv.sambasivan@arangodb.com>
2022-05-13 13:40:21 +0530

    Adding data and src folders.

---
+++ src/download_data.py
@@ -0,0 +1,6 @@
+import requests, zipfile, io, os
+r = requests.get('https://files.grouplens.org/datasets/movielens/ml-100k.zip')
+z = zipfile.ZipFile(io.BytesIO(r.content))
+path_parent = os.path.dirname(os.getcwd())
+data_dir = path_parent + '/data'
+z.extractall(data_dir)
